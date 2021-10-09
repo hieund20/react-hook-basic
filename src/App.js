59ee289c7230226e888dd0196769823c0b2a7709
hieund20 <hieund898@gmail.com>
2021-10-09 11:41:08 +0700

    upload TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
 import './App.scss';
-import ColorBox from './components/ColorBox/index.jsx';
+import TodoList from './components/TodoList/index.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Learn react js' },
+    { id: 2, title: 'Build project' },
+    { id: 3, title: 'Learn guitar' }
+  ]);
+
+  function handleTodoList(todo) {
+    console.log(todo);
+    //if x.id === to.id, index will be equal this index
+    //if x.id !== to.id, index will be equal -1
+    const index = todoList.findIndex(x => x.id === todo.id);
+
+    if (index < 0) return; //do not something
+
+    //Clone todoList before update
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1); //Remove todo
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h3>useState Lab</h3>
-      <ColorBox />
+      <h3>Todo List</h3>
+      <TodoList todos={todoList}
+        onTodoClick={handleTodoList} />
     </div>
   );
 }
